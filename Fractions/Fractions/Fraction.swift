921a4fdcc526dfb4f0b674708cf06cc2dbef15a8
James <jchalmers811@gmail.com>
2018-07-11 21:55:43 +1200

    Initialiser and designated initialiser working

--- Fractions/Fractions/Fraction.swift
+++ Fractions/Fractions/Fraction.swift
@@ -15,9 +15,20 @@ class Fraction {
     var num: Int //numerator
     var den: Int //demonominator
     
+    //default initialiser
     init() {
         self.num = 0 //initialise fraction to zero
         self.den = 1
+    }
+    
+    //designated initialiser
+    init(num : Int, den : Int) {
+        
+        // Check the denominator...
+        assert(den != 0, "Denominator cannot be zero")
+        
+        self.num = num
+        self.den = den
     }
     
 }
--- Fractions/Fractions/main.swift
+++ Fractions/Fractions/main.swift
@@ -17,3 +17,30 @@ u = Fraction.init()
 // Show the state of the object's internal variables
 print("u=\(u.num)/\(u.den)")
 
+// Change the values of object's internal variables
+u.num = 2
+u.den = 3
+
+// Show the state of the object's internal variables
+print("u=\(u.num)/\(u.den)")
+
+// Define and create a new instance of a Fraction object
+var x: Fraction = Fraction()
+x.num = -5
+x.den = 1
+
+print("x=\(x.num)/\(x.den)")
+
+// Create and initialise a new Fraction object
+var u2: Fraction
+
+// Initialise the object
+u2 = Fraction(num: 2,den: 3)
+
+// Show the state of the object's internal variables
+print("u2=\(u2.num)/\(u2.den)")
+
+// Define and create a new instance of a Fraction object
+var x2: Fraction = Fraction(num: -5,den: 1)
+
+print("x2=\(x2.num)/\(x2.den)")
