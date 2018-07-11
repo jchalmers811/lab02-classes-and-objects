3830277c402e5168453fadb04420ac6c4e9d4fba
James <jchalmers811@gmail.com>
2018-07-11 21:43:39 +1200

    Set up basic structure of fraction class and called from main method

---
+++ Fractions/Fractions/Fraction.swift
@@ -0,0 +1,23 @@
+//
+//  Fraction.swift
+//  Fractions
+//
+//  Represent a number as a fraction of two integers
+//
+//  Created by James on 11/07/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import Foundation
+
+class Fraction {
+    
+    var num: Int //numerator
+    var den: Int //demonominator
+    
+    init() {
+        self.num = 0 //initialise fraction to zero
+        self.den = 1
+    }
+    
+}
---
+++ Fractions/Fractions/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Fractions
+//
+//  Created by James on 11/07/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import Foundation
+
+// Define a new Fraction object
+var u: Fraction
+
+// Create and initialise a new object instance
+u = Fraction.init()
+
+// Show the state of the object's internal variables
+print("u=\(u.num)/\(u.den)")
+
