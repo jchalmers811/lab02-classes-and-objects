eb5a8c5ece38b665932a9dfff3bb6cef079efd6b
James <jchalmers811@gmail.com>
2018-07-11 22:30:22 +1200

    Added functions, static functions, overwritten functions and operator overwrites

--- Fractions/Fractions/Fraction.swift
+++ Fractions/Fractions/Fraction.swift
@@ -71,4 +71,176 @@ class Fraction {
     
     
     
+    // METHODS
+    
+    /**
+     Adds a fraction to self.
+     
+     - parameter f: Fraction to add to self
+     
+     - returns: Fraction The result of adding f to self.
+     */
+    func add(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den + self.den*f.num,
+                        den: self.den*f.den)
+    }
+    
+    /**
+     Subtracts fraction from self.
+     
+     - parameter f: Fraction to subtract from self
+     
+     - returns: Fraction The result of subtracting f from self.
+     */
+    func subtract(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den - self.den*f.num,
+                        den: self.den*f.den)
+    }
+    
+    /**
+     Multiplies self by a fraction.
+     
+     - parameter f: Fraction to multiply self by
+     
+     - returns: Fraction The result of multiplying self by f.
+     */
+    func multiply(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.num, den: self.den*f.den)
+    }
+    
+    /**
+     Divides self by a fraction.
+     
+     - parameter f: Fraction to divide self by
+     
+     - returns: Fraction The result of dividing self by f.
+     */
+    func divide(_ f: Fraction) -> Fraction {
+        return Fraction(num: self.num*f.den, den: self.den*f.num)
+    }
+    
+    /**
+     Add a fraction to fraction.
+     
+     - parameter f1: Fraction to add to
+     - parameter f2: Fraction to be added
+     
+     - returns: The result of f1 + f2.
+     */
+    static func add(_ f1: Fraction, to f2: Fraction) -> Fraction {
+        return Fraction(num: f1.num*f2.den + f1.den*f2.num,
+                        den: f1.den*f2.den)
+    }
+    
+    /**
+     Subtract a fraction from fraction.
+     
+     - parameter f1: Fraction to subtract
+     - parameter f2: Fraction to subtract from
+     
+     - returns: The result of f2 - f1.
+     */
+    static func subtract(_ f1: Fraction, from f2: Fraction) -> Fraction {
+        return f2.subtract(f1);
+    }
+    
+    /**
+     Multiply a fraction by fraction.
+     
+     - parameter f1: Fraction to multiply
+     - parameter f2: Fraction to multiply by
+     
+     - returns: The result of f1*f2.
+     */
+    static func multiply(_ f1: Fraction, by f2: Fraction) -> Fraction {
+        return f1.multiply(f2)
+    }
+    
+    /**
+     Divide a fraction by fraction.
+     
+     - parameter f1: Fraction to divide
+     - parameter f2: Fraction to divide by
+     
+     - returns: The result of f1/f2.
+     */
+    static func divide(_ f1: Fraction, by f2: Fraction) -> Fraction {
+        return f1.divide(f2)
+    }
+    
+    /**
+     Adds an integer to self.
+     
+     - parameter x: Integer to add to self
+     
+     - returns: Fraction The result of adding x to self.
+     */
+    func add(_ x: Int) -> Fraction {
+        return Fraction(num: self.num + self.den*x,
+                        den: self.den)
+    }
+    
+    /**
+     Subtracts an integer from self.
+     
+     - parameter x: Integer to subtract from self
+     
+     - returns: Fraction The result of subtracting x from self.
+     */
+    func subtract(_ x: Int) -> Fraction {
+        return Fraction(num: self.num - self.den*x,
+                        den: self.den)
+    }
+    
+    /**
+     Multiplies self by an integer.
+     
+     - parameter x: Integer to multiply self by
+     
+     - returns: Fraction The result of multiplying self by x.
+     */
+    func multiply(_ x: Int) -> Fraction {
+        return Fraction(num: self.num*x, den: self.den)
+    }
+    
+    /**
+     Divides self by an integer.
+     
+     - parameter x: Integer to divide self by
+     
+     - returns: Fraction The result of dividing self by x.
+     */
+    func divide(_ x: Int) -> Fraction {
+        return Fraction(num: self.num, den: self.den*x)
+    }
+    
+}
+
+/**
+ + operator between two Fractions
+ */
+func +(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.add(f2)
+}
+
+/**
+ - operator between two Fractions
+ */
+func -(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.subtract(f2)
+}
+
+/**
+ * operator between two Fractions
+ */
+func *(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.multiply(f2)
+}
+
+/**
+ / operator between two Fractions
+ */
+
+func /(f1: Fraction, f2: Fraction) -> Fraction {
+    return f1.divide(f2)
 }
--- Fractions/Fractions/main.swift
+++ Fractions/Fractions/main.swift
@@ -24,5 +24,28 @@ var x: Fraction = Fraction(num: -5)
 print("x=\(x.description)")
 print("decimal value of x=\(x.decimal)")
 
+var v = Fraction(num: -7, den: 9)
 
+print("v=\(v.description)")
+print("decimal value of v=\(v.decimal)")
 
+var f: Fraction
+
+f = u+v
+print("(" + u.description + ")+(" + v.description + ")=" + f.description)
+
+f = v-u
+print("(" + u.description + ")-(" + v.description + ")=" + f.description)
+
+f = u*v
+print("(" + u.description + ")*(" + v.description + ")=" + f.description)
+
+f = u/v
+print("(" + u.description + ")/(" + v.description + ")=" + f.description)
+
+f = u.add(3)
+print("(" + u.description + ")+3=" + f.description)
+
+let z: Int = 2
+f = u.divide(z)
+print("(" + u.description + ")/\(z)=" + f.description)
